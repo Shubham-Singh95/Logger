f353bbb7f4b8cbe9be15df0c0972bb55452bab83
SHUBHAM SINGH <103556175+Shubham-Singh95@users.noreply.github.com>
2022-05-01 00:52:01 +0530

    Update File_logger.py

--- File_logger.py
+++ File_logger.py
@@ -12,39 +12,23 @@ def dlp_logger():
 
     "Logs in the events into a CSV file that are sent by LoggingEventHandler"
 
-    # Initializes the format of the logs
-
     logging.basicConfig(filename='event_logger.csv',
                         level=logging.NOTSET,
                         format='%(asctime)s, %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-   
-    # Creates a logger named Event_Logger
+                        datefmt='%Y-%m-%d %H:%M:%S')  # Initializes the format of the logs
 
-    logger = logging.getLogger('event_logger')
+    logger = logging.getLogger('event_logger') # Creates a logger named Event_Logger
+    handler = RotatingFileHandler("event_logger.csv", maxBytes=2000, backupCount=2) # Switches from one file to the next when the current file reaches a 2000 byte
+    logger.addHandler(handler)  # Parses the handler to the test logger
 
-    # Switches from one file to the next when the current file reaches a 2000 bytes
+    event_handler = LoggingEventHandler() # Creating the instance of Class LoggingEventHandler
     
-    handler = RotatingFileHandler("event_logger.csv", maxBytes=2000, backupCount=2)
-
-    # Parses the handler to the test logger
-
-    logger.addHandler(handler)
-
-    # Creating the instance of Class LoggingEventHandler
-
-    event_handler = LoggingEventHandler()
-
-    # Observer polls the changes made the in the directory
-
-    observer = Observer()
+    observer = Observer()  # Observer polls the changes made the in the directory
     observer.schedule(event_handler, path='/', recursive=True)
     observer.start()
     try:
         while True:
-            # Re-triggers the logging every sec , thus sleep(1)
-
-            time.sleep(1)
+            time.sleep(1)  # Re-triggers the logging every sec , thus sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
