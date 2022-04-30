a8f2f7871bf0f7be9316b9650d24674cb78a3abc
SHUBHAM SINGH <103556175+Shubham-Singh95@users.noreply.github.com>
2022-04-30 21:18:39 +0530

    Add files via upload

--- File_logger.py
+++ File_logger.py
@@ -3,57 +3,45 @@ import time
 import logging
 from logging.handlers import RotatingFileHandler
 from watchdog.observers import Observer
+
+# To log all the events captured
 from watchdog.events import LoggingEventHandler
 
 
-class CustomLoggingEventHandler(LoggingEventHandler):
-    """Logs all the events captured."""
-
-    def __init__(self, logger):
-        self.logger = logger
-
-    def on_moved(self, event):
-        super().on_moved(event)
-
-        what = 'directory' if event.is_directory else 'file'
-        self.logger.info("Moved %s: from %s to %s", what, event.src_path,
-                     event.dest_path)
-
-    def on_created(self, event):
-        super().on_created(event)
-
-        what = 'directory' if event.is_directory else 'file'
-        self.logger.info("Created %s: %s", what, event.src_path)
-
-    def on_deleted(self, event):
-        super().on_deleted(event)
-
-        what = 'directory' if event.is_directory else 'file'
-        self.logger.info("Deleted %s: %s", what, event.src_path)
-
-    def on_modified(self, event):
-        super().on_modified(event)
-
-        what = 'directory' if event.is_directory else 'file'
-        self.logger.info("Modified %s: %s", what, event.src_path)
-
-
 def dlp_logger():
+
+    "Logs in the events into a CSV file that are sent by LoggingEventHandler"
+
+    # Initializes the format of the logs
+
     logging.basicConfig(filename='test.csv',
-                        level=logging.DEBUG,
+                        level=logging.NOTSET,
                         format='%(asctime)s, %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
+    # Creates a logger named test
 
     logger = logging.getLogger('test')
+
+    # Switches from one file to the next when the current file reaches a 2000 bytes
     handler = RotatingFileHandler("test.csv", maxBytes=2000, backupCount=2)
+
+    # Parses the handler to the test logger
+
     logger.addHandler(handler)
 
+    # Creating the instance of Class LoggingEventHandler
+
     event_handler = LoggingEventHandler()
+
+    # Observer polls the changes made the directory
+
     observer = Observer()
     observer.schedule(event_handler, path='C:/Users/Asus/Desktop/restAPI', recursive=True)
     observer.start()
     try:
         while True:
+            # Re-triggers the logging every sec , thus sleep(1)
+
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
